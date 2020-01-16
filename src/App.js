d58921b4e5b6483756b48d98201199b8defb9e6e
cedric <464231692@qq.com>
2020-01-16 01:35:35 -0500

    menu

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './app.css';
 import Intro from './pages/intro';
 import Works from './pages/works';
 import Contact from './pages/contact';
-import {Link, Route, NavLink} from 'react-router-dom'
+import {Link, Route, NavLink} from 'react-router-dom';
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded.js';
 
 export default function App(){
   
@@ -14,8 +15,9 @@ export default function App(){
         <img src={require('./assets/logo.png')}></img>
         
         <div>
-          
-          <NavLink to="/home" className={styles.navlink} activeStyle={{color:'white'}}>Home</NavLink>  
+          <MenuRoundedIcon className={styles.menu} style={{fill: "white", fontSize: 40}} />
+
+          <NavLink to="/home" className={styles.navlink} activeStyle={{color:'white'}}>About</NavLink>  
           <NavLink to="/works" className={styles.navlink} activeStyle={{color:'white'}}>Works</NavLink>
           <NavLink to="/contact" className={styles.navlink} activeStyle={{color:'white'}}>Contact</NavLink> 
         </div>
@@ -26,6 +28,8 @@ export default function App(){
         <Route path="/works" exact component={Works}></Route>
         <Route path="/contact" exact component={Contact}></Route>
       </main>
+
+      
       
       <footer>
         Tianwei Wang © 2020
